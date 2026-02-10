c6834ea4bac539771bae60f2ecfa136bb1936594
Tech Rules <66055840+techrules07@users.noreply.github.com>
2026-02-10 19:40:05 +0530

    invoice updated

--- src/main/java/com/smartstay/reports/responses/invoice/TemplateInfo.java
+++ src/main/java/com/smartstay/reports/responses/invoice/TemplateInfo.java
@@ -4,6 +4,7 @@ public record TemplateInfo(String colorCode,
                            String hostelLogo,
                            String phone,
                            String emailId,
+                           String signature,
                            String termsAndCondition,
                            String notes) {
 }
