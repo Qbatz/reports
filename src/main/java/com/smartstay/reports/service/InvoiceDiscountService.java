37a1c6ce21e99ab1b246f96c2bc0caa756fd7897
Tech Rules <66055840+techrules07@users.noreply.github.com>
2026-05-07 19:36:11 +0530

    invoice v1 modifications

---
+++ src/main/java/com/smartstay/reports/service/InvoiceDiscountService.java
@@ -0,0 +1,20 @@
+package com.smartstay.reports.service;
+
+import com.smartstay.reports.dao.InvoiceDiscounts;
+import com.smartstay.reports.repositories.InvoiceDiscountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InvoiceDiscountService {
+
+    @Autowired
+    private InvoiceDiscountRepository invoiceDiscountRepository;
+    public double getInoiceDiscount(String hostelId, String invoiceId) {
+        InvoiceDiscounts invoiceDiscounts = invoiceDiscountRepository.findByHostelIdAndInvoiceId(hostelId, invoiceId);
+        if (invoiceDiscounts != null) {
+            return invoiceDiscounts.getDiscountAmount();
+        }
+        return 0.0;
+    }
+}
